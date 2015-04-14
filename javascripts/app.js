663b90c7957bdcc7d37c31fbd5a1fcf15dbfdcd8
Erica Johnson <edj@uoregon.edu>
2015-04-14 16:36:10 -0700

    Added a link from index to the reasearch.html file from assignment 1

---
+++ javascripts/app.js
@@ -0,0 +1,87 @@
+var main = function () {
+    var toDos = ["Get groceries",
+                 "Make up some new ToDos",
+                 "Prep for Monday's class",
+                 "Answer emails",
+                 "Take Gracie to the park",
+                 "Finish writing this book"];
+
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
+
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button,
+                i;
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                // newest first, so we have to go through
+                // the array backwards
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                // oldest first, so we go through the array forwards
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                // input a new to-do
+                $input = $("<input>"),
+                $button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input).append($button);
+               /* Alternatively append() allows multiple arguments so the above
+                can be done with $content = $("<div>").append($input, $button); */
+            } else if ($('#search').attr('id')) {
+                // input a search keyword
+                $input = $("<input>"),
+                $button = $("<button>").text("Search");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        //loop through toDos, look for a match on first element
+                        var found = false;
+                        toDos.forEach( function ( item ) {
+                            //item = "wash the car" e.g.
+                            if ( item.search($input.val()) != -1) {
+                                window.alert("Found it: " + item);
+                                found = true;
+                                //break;
+                            }
+                        });
+                        if ( !found ) {
+                            window.alert("Not Found.");
+                        }
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input, $button);
+            }
+
+            $("main .content").append($content);
+
+            return false;
+        });
+    });
+
+    $(".tabs a:first-child span").trigger("click");
+};
+
+$(document).ready(main);
